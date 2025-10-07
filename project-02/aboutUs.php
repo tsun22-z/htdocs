74aee5b4d86823640171395f920b627d59b2d922
Luis Rodriguez <luis.rod24ed@gmail.com>
2025-10-07 10:07:05 -0500

    Added Project 2

---
+++ project-02/aboutUs.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Project-01 Home Page </title>
+</head>
+<body>
+
+<?php 
+    include("./includes/nav.php");
+?>
+
+
+
+<h1 style="text-align:center"> Welcome to my Project-02 About Us Page </h1>
+
+<?php 
+    include("./includes/nav.php");
+?>
+
+</body>
+</html>
---
+++ project-02/includes/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+         <li><a href="index.php">Home</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+            <li><a href="contactUs.php">Contact Us</a></li>
+            <li><a href="FAQ.php">FAQ</a></li>
+    </ul>
+</nav>
