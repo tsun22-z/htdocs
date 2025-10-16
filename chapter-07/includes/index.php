87608068a687473841d5e177b2a3af15bd93d256
Luis Rodriguez <luis.rod24ed@gmail.com>
2025-10-16 10:31:27 -0500

    added php modular to website

--- chapter-07/includes/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('./includes/head.php');
-include('./includes/meta.php');
-include('./includes/topNav.php');
-include('./includes/hero.php');
-include('./includes/blog.php');
-include('./includes/partners.php');
-include('./includes/bottomNav.php');
-
---
+++ chapter-07/index.php
@@ -0,0 +1,16 @@
+<?php
+include("./includes/head.php");
+include("./includes/meta.php");
+include("./includes/topNav.php");
+include("./includes/hero.php");
+include("./includes/blog.php");
+include("./includes/partners.php");
+include("./includes/bottomNav.php");
+//include('./includes/head.php');
+//include('./includes/meta.php');
+//include('./includes/topNav.php');
+//include('./includes/hero.php');
+//include('./includes/blog.php');
+//include('./includes/partners.php');
+//include('./includes/bottomNav.php');
+?>
